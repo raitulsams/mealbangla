01755dbec9859828327464736767c17384b44dea
raitulsams <raitulsams202@gmail.com>
2025-06-04 08:27:01 +0200

    go back button added

--- src/components/Filtered Meals/FilteredMeals.jsx
+++ src/components/Filtered Meals/FilteredMeals.jsx
@@ -10,7 +10,7 @@ const FilteredMeals = () => {
     return (
         <>
             <div class=" bg-base-100 shadow-sm p-4">
-                <button onClick={() => { navigate(-1) }} class="flex items-center justify-center w-full px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700">
+                <button onClick={() => { navigate(-1) }} class="flex items-center justify-center w-full px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white rounded-lg gap-x-2 sm:w-auto dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 rtl:rotate-180">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
                     </svg>
